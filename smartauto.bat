450d9a420f8704326e56de3536c6a9c4d84e9a2d
unknown <zhangkeluo@cn.net.ntes>
2019-01-09 13:50:12 +0800

    添加作业编辑保存

--- smartauto.bat
+++ smartauto.bat
@@ -1,6 +1,6 @@
 @echo off
-set SMARTAUTO_PATH=C:\Users\zhangkeluo\Desktop\SmartAuto\win-unpacked\resources\app.asar.jar\common
-set JAVA_EXEC=C:\Users\zhangkeluo\Desktop\SmartAuto\win-unpacked\resources\app.asar.jar\common\jre1.8.0_144\bin\java.exe
+set SMARTAUTO_PATH=C:\Users\zhangkeluo\Desktop\SmartAuto\app.asar.jar\common
+set JAVA_EXEC=C:\Users\zhangkeluo\Desktop\SmartAuto\app.asar.jar\common\jre1.8.0_144\bin\java.exe
 set CMD="%JAVA_EXEC%" -Dfile.encoding=UTF-8 -jar "%SMARTAUTO_PATH%\smartauto.jar" "%*"
 echo %CMD%
 %CMD%
